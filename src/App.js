81d0edc3785823e3f9639e383264ae6ed3fa549e
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-25 16:05:15 -0700

    manual react-config with webpack and babel

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component, Fragment} from "react";
+import Apptwo from './Apptwo';
+import "./App.scss";
+import logo from "./assets/logo.svg";
+
+class App extends Component {
+
+    render() {
+
+        return (
+
+            <Fragment>
+            <header className = "header">
+            <div>
+                <img src={logo}  />
+            </div>
+                <div>
+                    <h1>Heading 1</h1>
+                </div>
+
+                <div>
+                    <p>Description: Hello World from webpack and babel</p>
+                </div>
+
+                <Apptwo />
+
+                </header>
+
+            </Fragment>
+
+        );
+    }
+}
+
+export default App;
---
+++ src/Apptwo.jsx
@@ -0,0 +1,23 @@
+import React, {Component, Fragment} from "react";
+
+class Apptwo extends Component {
+
+    render() {
+
+        return (
+
+            <Fragment>
+                <div>
+                    <h1>Heading Header 2</h1>
+                </div>
+
+                <div>
+                    <p>Description: Hello World 2 from webpack and babel</p>
+                </div>
+            </Fragment>
+
+        );
+    }
+}
+
+export default Apptwo;
