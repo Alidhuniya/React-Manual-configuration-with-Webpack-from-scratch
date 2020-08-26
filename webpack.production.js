bb3ec26f87a087168bc10b775c2eb5118fddbb97
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-26 10:31:56 -0700

    pwa added back

--- webpack.production.js
+++ webpack.production.js
@@ -2,13 +2,13 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
-// const WorkboxPlugin = require('workbox-webpack-plugin'); 
-// const CopyPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin'); 
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = merge(common, {
-  mode: 'production', // solved from here: https://github.com/webpack/webpack/issues/3486
-  performance: { hints: false },
+  mode: 'production',
+  performance: { hints: false },  // solved from here: https://github.com/webpack/webpack/issues/3486
   output: {
    
     path: path.resolve(__dirname, 'dist'), // this will open index.html file in dist folder by default
@@ -45,19 +45,19 @@ module.exports = merge(common, {
 
    
 
-    // new WorkboxPlugin.GenerateSW({
+    new WorkboxPlugin.GenerateSW({
            // these options encourage the ServiceWorkers to get in there fast
           // and not allow any straggling "old" SWs to hang around
-    //        clientsClaim: true,
-    //        skipWaiting: true
-    //     }),
+           clientsClaim: true,
+           skipWaiting: true
+        }),
 
-    //     new CopyPlugin({
-    //       patterns: [
-    //         { from: './public/manifest', to: 'manifest' }
+        new CopyPlugin({
+          patterns: [
+            { from: './public/manifest', to: 'manifest' }
             
-    //       ],
-    //     }),
+          ],
+        }),
 
     
   ],
